ca4b53797a039ee88189e2f107266b7a70e56e66
khaciezi <55909503+khaciezi@users.noreply.github.com>
2020-12-15 17:12:34 -0800

    Delete disconnect.js

--- disconnect.js
+++
@@ -1,8 +0,0 @@
-module.exports = {
-    name: 'disconnect',
-    aliases: ['dc', 'leave'],
-    description: 'disconnects',
-    execute(message) {
-        message.channel.send('Disconnected.');
-    },
-};
